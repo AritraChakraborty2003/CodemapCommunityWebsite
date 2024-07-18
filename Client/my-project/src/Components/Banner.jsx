2e77ad516c2d38540d4cb5a482ea0fbc731ce6a2
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-07-18 13:59:12 +0530

    update:updated the banner and navbar component

--- Client/my-project/src/Components/Banner.jsx
+++ Client/my-project/src/Components/Banner.jsx
@@ -1,32 +1,49 @@
 const Banner = () => {
   return (
     <>
-      <div className=" w-[100vw] pb-5 overflow-x-hidden bg-bgColor flex justify-center items-center flex-col gap-y-4">
-        <div className="flex gap-x-2 items-center h-[10vmin]">
-          <div className="h-[4px] w-[19px] border-grey border-b-[1px]"></div>
-          <p className="text-[3.65vmin] font-medium font-poppins">
-            The Technical Community to nurture your skills
+      <div className="h-[85vh] w-[100vw] pb-5 overflow-x-hidden bg-bgColor flex justify-center items-center flex-col gap-y-4">
+        <div className="mt-[-6vmin] lg:mt-0 flex gap-x-2 items-center h-[20vmin]  md:h-[25vmin] lg:h-[10vmin] ml-2 lg:ml-0">
+          <div className="h-[4px] w-[19px] border-grey border-b-[1px] hidden lg:block"></div>
+          <p className="text-center mt-[-2vmin] text-[4vmin] md:text-[3.3vmin] lg:text-[4vmin] lg:font-medium font-poppins">
+            Your own Technical Community to develop and nurture your skills
           </p>
         </div>
-        <p className="text-[8vmin] lg:text-[10vmin] font-medium font-poppins">
+        <p className="text-center text-[14vmin] md:text-[13vmin] mt-[-4vmin] lg:mt-[-1vmin] font-bold font-poppins">
           CodeMap Community
         </p>
-        <p className="text-[3.45vmin] lg:text-[3vmin]  font-poppins mt-[-2.5vmin]">
+        <p className="text-[4.25vmin] md:text-[3.75vmin] lg:text-[4vmin]  font-poppins mt-[-2.5vmin]">
           ( Where Ideas Transform Into Innovation )
         </p>
 
         <div className="flex gap-x-2 items-center h-[10vmin]">
           <div className="hidden md:block h-[4px] w-[17px] border-bannerGrey border-b-[2px] mt-[3vmin]"></div>
-          <p className="lg:ml-[0.45vmin] text-[4.15vmin] lg:text-[3.45vmin] font-poppins text-bannerGrey mt-[3vmin]">
-            Learn - Participate - Groww - Innovate
+          <p className="lg:ml-[0.45vmin] text-[4.65vmin] md:text-[3.85vmin] lg:text-[3.65vmin] font-poppins text-bannerGrey mt-[3vmin]">
+            Learn - Participate - Innovate - Grow
           </p>
         </div>
 
-        <div className="buttonHolder">
+        <div className="hidden lg:block buttonHolder">
           <div className="buttonHolder h-[23vmin] lg:h-[15vmin]  flex items-center justify-center">
-            <button className="font-poppins 2xl:w-[20vmin] bg-btnColor p-2 pl-3 pr-3 lg:p-3 lg:pr-4 lg:pl-4 rounded-lg text-white mt-[-3vmin] md:mt-0">
+            <button className="text-[4.65vmin] lg:text-[3vmin] font-poppins 2xl:w-full bg-btnColor p-2 pl-3 pr-3 lg:p-3 lg:pr-4 lg:pl-4 rounded-lg text-white mt-[-3vmin] md:mt-0">
               Become Campus Lead
             </button>
+          </div>
+        </div>
+
+        <div className="block lg:hidden buttonHolder">
+          <div className="buttonHolder h-[23vmin] lg:h-[15vmin]  flex items-center justify-center">
+            <button className="text-[5vmin] md:text-[3.75vmin] lg:text-[3vmin] font-poppins 2xl:w-[20vmin] bg-btnColor p-2 pl-3 pr-3 lg:p-3 lg:pr-4 lg:pl-4 rounded-lg text-white mt-[-3vmin] md:mt-0">
+              LMS Solution
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-center items-center md:mt-[-2vmin]">
+          <div className="downArrowHolder block md:hidden h-[15vmin] w-[15vmin] md:h-[10vmin] md:w-[10vmin] ">
+            <img
+              src="./da1.png"
+              alt=""
+              className="src max-w-full max-h-full object-contain"
+            />
           </div>
         </div>
       </div>
--- Client/my-project/src/Components/Navbar.jsx
+++ Client/my-project/src/Components/Navbar.jsx
@@ -11,8 +11,8 @@ const Navbar = () => {
   return (
     <>
       <div className="holderNav bg-bgColor pb-2">
-        <div className="navBarBox   md:h-[15vmin] w-[100vw] bg-bgColor flex ">
-          <div className="logoHolder h-[23vmin] lg:h-[15vmin]  flex pt-2 lg:pt-0">
+        <div className="navBarBox w-[100vw] bg-bgColor flex ">
+          <div className="logoHolder lg:h-[15vmin]  flex pt-2 lg:pt-0">
             <div className="iconHolder h-[17vmin] w-[17vmin] md:h-[10vmin] md:w-[10vmin] lg:h-[14vmin] lg:w-[14vmin]  mt-1 lg:mt-1 md:mt-4 ml-2 2xl:ml-4 2xl:mt-3 flex justify-center items-center pt-1">
               <img
                 src="./CMLogo.png"
@@ -20,7 +20,7 @@ const Navbar = () => {
               ></img>
             </div>
             <div className="flex font-semibold items-center textHolder h-[12vmin] w-[51vw] md:w-[67vw] lg:h-[10vmin] lg:w-full mt-4 lg:mt-4 ml-2 2xl:mt-5">
-              <p className=" text-[5vmin] md:text-[4vmin] 2xl:text-[3.75vmin] ml-1 font-poppins ">
+              <p className=" text-[5vmin] md:text-[4vmin] 2xl:text-[3.75vmin] ml-1 font-poppins mt-[2vmin]">
                 Code
                 <span className="md:text-[4vmin] 2xl:text-[3.75vmin]">Map</span>
               </p>
@@ -59,7 +59,7 @@ const Navbar = () => {
           </div>
           <div className="hidden lg:block">
             <div className="buttonHolder h-[23vmin] lg:h-[15vmin]  flex items-center justify-center">
-              <button className="w-[30vmin] 2xl:w-[20vmin] bg-btnColor p-3 rounded-lg text-white">
+              <button className="w-[30vmin] 2xl:w-full   bg-btnColor p-3 rounded-lg text-lg text-white 2xl:text-[2.45vmin] 2xl:p-4">
                 LMS Platform
               </button>
             </div>
@@ -78,7 +78,7 @@ const Navbar = () => {
         <div className="line w-[96vw] ml-[2vw] mr-[3vw] h-[10px] border-lightGrey border-t-2 mt-2 lg:mt-0"></div>
       </div>
       {displayNavHam === true && (
-        <div className="navHamMobile block lg:hidden w-[100vw] bg-bgColor  pl-3 pb-5 ">
+        <div className="navHamMobile block lg:hidden w-[100vw] bg-bgColor  pl-3 pb-6 ">
           <ul className="space-y-[3vmin] text-lg md:text-[3vmin]">
             <li>About-us</li>
             <li>Support</li>
@@ -91,7 +91,7 @@ const Navbar = () => {
             <li>Contact</li>
             <li>
               <button className=" 2xl:w-[20vmin] bg-btnColor p-3 rounded-lg text-white ml-[-0.5vmin]">
-                LMS Solution
+                Become Campus Lead
               </button>
             </li>
           </ul>
