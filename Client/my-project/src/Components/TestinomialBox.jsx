e9ed343d123d381732627c23023d7a1428a54c7f
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-07-22 00:00:39 +0530

    update:updated the Testinomial Box Component

--- Client/my-project/src/Components/TestinomialBox.jsx
+++ Client/my-project/src/Components/TestinomialBox.jsx
@@ -3,8 +3,8 @@ import { Rating } from "react-simple-star-rating";
 const TestinomialBox = (props) => {
   return (
     <>
-      <div className="boxArea bg-white flex flex-col w-[100vw] md:w-[85vmin] border-[1px] pl-3 pt-[1px] rounded-lg shadow-2xl">
-        <div className="row1 flex w-[98vw] pb-1 ">
+      <div className="boxArea bg-white flex flex-col w-[85vmin] border-[1px] pl-3 pt-[1px] rounded-lg shadow-2xl">
+        <div className="row1 flex w-[85vmin] pb-1 ">
           <div className="col1">
             <div className="imgBox w-[19vmin] h-[14vmin]  mt-4 ml-2">
               <img
@@ -14,7 +14,7 @@ const TestinomialBox = (props) => {
             </div>
           </div>
           <div className="col2 mt-2">
-            <div className="font-poppins space-y-1 detailsBox w-[97vw]  mt-1 text-[2.45vmin]">
+            <div className="font-poppins space-y-1 detailsBox w-[85vmin]  mt-1 text-[2.45vmin]">
               <p className="text-btnColor text-[3.5vmin] md:text-[2.65vmin]">
                 <b>Name:</b>
                 <span className="text-black font-medium text-[3.5vmin] md:text-[2.7vmin]">
@@ -54,7 +54,7 @@ const TestinomialBox = (props) => {
             </div>
           </div>
         </div>
-        <div className=" flex font-poppins w-[70vmin] row2 lg:w-[80vmin] h-[25vmin] lg:letter-spacing-[1px] lg:ml-1 border-black border-t-[1px] pt-4 lg:p-2 font-medium">
+        <div className=" flex font-poppins w-[65vmin] row2 lg:w-[80vmin] h-[25vmin] lg:letter-spacing-[1px] lg:ml-1 border-black border-t-[1px] pt-4 lg:p-2 font-medium">
           <p className="mt-2 text-[3.7vmin] md:text-[3.15vmin] md:pl-2 lg:text-[2.8vmin] lg:tracking-[0.17vmin]">
             " {props.data.comment} "
           </p>
