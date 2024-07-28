57ee3615e8d3fc081a5f972cbd4a1718b3cea498
Aritra Chakraborty <107548404+AritraChakraborty2003@users.noreply.github.com>
2024-07-29 00:21:52 +0530

    Update SyllabusDisplay.jsx

--- Client/my-project/src/Components/SyllabusDisplay.jsx
+++ Client/my-project/src/Components/SyllabusDisplay.jsx
@@ -1,10 +1,10 @@
 const SyllabusDisplay = (props) => {
   return (
     <>
-      {props.data.map((val) => (
+      {props.data.map((val,ind) => (
         <>
           <div className="ml-3 pb-5">
-            <p className="text-5xl font-bold pb-2">{val.name}</p>
+            <p className="text-5xl font-bold pb-2">{ind+1}{" "+val.name}</p>
             <p className="text-2xl font-medium">Role: {val.role}</p>
             <p className="text-2xl ">Topic 1: {val.topic1}</p>
             <p className="text-2xl ">Topic 2: {val.topic2}</p>
