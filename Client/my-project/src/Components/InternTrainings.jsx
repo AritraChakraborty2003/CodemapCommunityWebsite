98b41c913da568cc6b614c8207dd6dfcc95ec3fa
Aritra Chakraborty <107548404+AritraChakraborty2003@users.noreply.github.com>
2024-07-29 00:53:15 +0530

    Update InternTrainings.jsx

--- Client/my-project/src/Components/InternTrainings.jsx
+++ Client/my-project/src/Components/InternTrainings.jsx
@@ -10,12 +10,13 @@ import { NonTechnicalSyllabus } from "./NonTechnicalSyllabus";
 const InternTraining = () => {
   const navigate = useNavigate();
   const [state1, setstate1] = useState(0);
+  const [state, setstate] = useState(0);
   return (
     <>
       {(localStorage.getItem("user") === "Admin2024" && (
         <>
           <NavbarCMS type="Interns" />
-          <div className="flex gap-x-7 mt-3 ml-4 2xl:ml-8 pb-10">
+          <div className="flex gap-x-[12vmin] mt-10 ml-4 2xl:ml-8 pb-5 justify-center items-center">
             <div>
               <p
                 className="text-[4vmin] 2xl:text-[2.75vmin] font-medium"
@@ -24,14 +25,14 @@ const InternTraining = () => {
                     "#000000";
                   document.getElementById("secLine1").style.backgroundColor =
                     "#D3D3D3";
-                  setstate1(0);
+                  setstate(0);
                 }}
               >
-                Technical
+                Training Syllabus
               </p>
               <div
                 id="firstLine1"
-                className="w-[19vmin] 2xl:w-[14vmin] h-[5px] bg-black mt-1"
+                className="w-[24vmin] 2xl:w-[14vmin] h-[5px] bg-black mt-1"
               ></div>
             </div>
 
@@ -44,20 +45,60 @@ const InternTraining = () => {
                     "#000000";
                   document.getElementById("firstLine1").style.backgroundColor =
                     "#D3D3D3";
+                  setstate(1);
+                }}
+              >
+                Videos
+                <div
+                  id="secLine1"
+                  className="w-[20vmin] 2xl:w-[18vmin] h-[5px] bg-lightGrey mt-1"
+                ></div>
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-x-7 mt-1 ml-4 2xl:ml-8 pb-5">
+            <div>
+              <p
+                className="text-[4vmin] 2xl:text-[2.75vmin] font-medium"
+                onClick={() => {
+                  document.getElementById("firstLine2").style.backgroundColor =
+                    "#000000";
+                  document.getElementById("secLine2").style.backgroundColor =
+                    "#D3D3D3";
+                  setstate1(0);
+                }}
+              >
+                Technical
+              </p>
+              <div
+                id="firstLine2"
+                className="w-[19vmin] 2xl:w-[14vmin] h-[5px] bg-black mt-1"
+              ></div>
+            </div>
+
+            <div id="code">
+              <p
+                id="showOpenings"
+                className="text-[4vmin] 2xl:text-[2.75vmin] font-medium"
+                onClick={() => {
+                  document.getElementById("secLine2").style.backgroundColor =
+                    "#000000";
+                  document.getElementById("firstLine2").style.backgroundColor =
+                    "#D3D3D3";
                   setstate1(1);
                 }}
               >
                 Non Technical
                 <div
-                  id="secLine1"
+                  id="secLine2"
                   className="w-[24vmin] 2xl:w-[18vmin] h-[5px] bg-lightGrey mt-1"
                 ></div>
               </p>
             </div>
           </div>
 
-          {state1 === 0 && <TechnicalSyllabus />}
-          {state1 == 1 && <NonTechnicalSyllabus />}
+          {state === 0 && state1 === 0 && <TechnicalSyllabus />}
+          {state === 0 && state1 == 1 && <NonTechnicalSyllabus />}
           <Footer />
         </>
       )) || <Navigate to="/InternLogin" />}
@@ -65,3 +106,4 @@ const InternTraining = () => {
   );
 };
 export default InternTraining;
+
