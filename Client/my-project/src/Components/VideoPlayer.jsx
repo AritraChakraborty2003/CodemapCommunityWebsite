e292696e689d0236f46c1d3ebe614b08edbbd542
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-08-03 16:23:39 +0530

    setup:setup commit

--- Client/my-project/src/Components/VideoPlayer.jsx
+++ Client/my-project/src/Components/VideoPlayer.jsx
@@ -5,25 +5,23 @@ const VideoPlayer = (props) => {
   const playerRef = useRef(null);
 
   return (
-<<<<<<< HEAD
     <div className="pb-10 flex justify-center flex-wrap items-center mt-5 gap-x-2 gap-y-4 pl-2 pr-3">
-=======
-    <div className="pb-10 flex justify-center flex-wrap items-center mt-5 gap-x-5 gap-y-10 pl-2 pr-3">
->>>>>>> ae3ac9388a08f5fc603d5c078bcdff404c298052
-      {props.data.map((val) => (
-        <>
-          <div>
-            <ReactPlayer
-              ref={playerRef}
-              url={val.link}
-              controls={true}
-              width={380}
-              height={230}
-            />
-            <p className="text-md md:text-lg font-bold mt-1">{val.name}</p>
-          </div>
-        </>
-      ))}
+      <div className="pb-10 flex justify-center flex-wrap items-center mt-5 gap-x-5 gap-y-10 pl-2 pr-3">
+        {props.data.map((val) => (
+          <>
+            <div>
+              <ReactPlayer
+                ref={playerRef}
+                url={val.link}
+                controls={true}
+                width={380}
+                height={230}
+              />
+              <p className="text-md md:text-lg font-bold mt-1">{val.name}</p>
+            </div>
+          </>
+        ))}
+      </div>
     </div>
   );
 };
