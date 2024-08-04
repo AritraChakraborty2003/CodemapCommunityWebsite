fd71ea60f5603c2c2e12492bcd982aa5ad6b47ea
Aritra Chakraborty <107548404+AritraChakraborty2003@users.noreply.github.com>
2024-08-04 15:01:48 +0530

    Update postAttempts.js

--- Backend/Controller/postAttempts.js
+++ Backend/Controller/postAttempts.js
@@ -4,13 +4,15 @@ const postAttempts = () => {
   return (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
-    const score=req.body.score;
+    const score = req.body.score;
+    const role = req.body.role;
     const attempted = "true";
 
     const attemptsObj = new attempts({
       name: name,
       email: email,
-      score:score,
+      score: score,
+      role: role,
       attempted: attempted,
     });
 
